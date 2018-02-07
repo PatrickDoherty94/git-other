a13a93b70ddfba0ec9ff68c20e5962e43bbc3c32
Patrick Doherty <csf15dop@SCIS.ULSTER.AC.UK>
2018-02-07 12:17:52 +0000

    update person

--- Person.java
+++ Person.java
@@ -1,6 +1,6 @@
-public class Person {  private string name; 
+public class Person {  private string name;  private int age; 
  
- public Person(String name)  { 
-  this.name = name;  } 
+ public Person(String name,int age)  {   this.name = name; 
+  this.age = age;  } 
  
- public void setName(String name)  {   this.name = name;  } } 
+ public void setName(String name)  {   this.name = name;  } }
