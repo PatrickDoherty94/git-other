04d9a718c94d955edc97092ef4b07d353066d3f4
Patrick Doherty <csf15dop@SCIS.ULSTER.AC.UK>
2018-02-07 12:16:35 +0000

    updated person

--- Person.java
+++ Person.java
@@ -1,6 +1,5 @@
 public class Person {  private string name; 
  
- public Person(String name)  { 
-  this.name = name;  } 
+ public Person(String name)  {   this.setName(name);  } 
  
- public void setName(String name)  {   this.name = name;  } } 
+ public void setName(String name) throws IllegalArguementException  {   if(name.isEmpty())    throw new IllegalArguementException(“Cant pass empty first name”);   this.name = name;  } }
